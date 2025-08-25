4718874615de146e07efbffb0845624103e92a00
whitedevil-141 <white.devil.dev.141@gmail.com>
2025-08-25 16:50:54 +0600

    add exception handling

--- api/routers/doctors.py
+++ api/routers/doctors.py
@@ -42,21 +42,25 @@ def create_doctor(
     file_location = os.path.join(UPLOAD_DIR, photo.filename)
     with open(file_location, "wb") as f:
         f.write(photo.file.read())
-
-    # Create DB entry
-    new_doc = Doctor(
-        name=name,
-        specialization=specialization,
-        category=category,
-        experience_yr=experience,
-        description=description,
-        phone=phone,
-        photo_url=f"../img/{photo.filename}"  # relative path for frontend
-    )
-    db.add(new_doc)
-    db.commit()
-    db.refresh(new_doc)
-    return new_doc
+    try:
+        # Create DB entry
+        new_doc = Doctor(
+            name=name,
+            specialization=specialization,
+            category=category,
+            experience_yr=experience,
+            description=description,
+            phone=phone,
+            photo_url=f"../img/{photo.filename}"  # relative path for frontend
+        )
+        db.add(new_doc)
+        db.commit()
+        db.refresh(new_doc)
+        return new_doc
+    except Exception as e:
+        db.rollback()
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.put("/update/{doctor_id}", response_model=DoctorOut)
 @limiter.limit("5/minute")
@@ -75,25 +79,29 @@ def update_doctor(
     doctor = db.query(Doctor).filter(Doctor.id == doctor_id).first()
     if not doctor:
         raise HTTPException(status_code=404, detail="Doctor not found")
+    try:
+        # Update fields
+        doctor.name = name
+        doctor.specialization = specialization
+        doctor.category = category
+        doctor.experience_yr = experience
+        doctor.description = description
+        doctor.phone = phone
 
-    # Update fields
-    doctor.name = name
-    doctor.specialization = specialization
-    doctor.category = category
-    doctor.experience_yr = experience
-    doctor.description = description
-    doctor.phone = phone
+        # Handle photo if provided
+        if photo:
+            file_location = os.path.join(UPLOAD_DIR, photo.filename)
+            with open(file_location, "wb") as f:
+                f.write(photo.file.read())
+            doctor.photo_url = f"../img/{photo.filename}"
 
-    # Handle photo if provided
-    if photo:
-        file_location = os.path.join(UPLOAD_DIR, photo.filename)
-        with open(file_location, "wb") as f:
-            f.write(photo.file.read())
-        doctor.photo_url = f"../img/{photo.filename}"
-
-    db.commit()
-    db.refresh(doctor)
-    return doctor
+        db.commit()
+        db.refresh(doctor)
+        return doctor
+    except Exception as e:
+        db.rollback()
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
 
 @router.delete("/{doctor_id}")
 @limiter.limit("3/minute")
