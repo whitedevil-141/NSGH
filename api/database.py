1a289409694c73505e75194b7c97bd74c4c99e79
whitedevil-141 <white.devil.dev.141@gmail.com>
2025-08-25 12:53:05 +0600

    update database url

--- api/database.py
+++ api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:@localhost/hospital_db"
+DATABASE_URL = "mysql+pymysql://admin:Admin@1234@localhost/hospital_db"
 
 
 engine = create_engine(DATABASE_URL)
