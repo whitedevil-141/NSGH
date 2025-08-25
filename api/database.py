f5a3e8508d9603874b732bd519df772b87cd0a01
whitedevil-141 <white.devil.dev.141@gmail.com>
2025-08-25 14:19:55 +0600

    update the db

--- api/database.py
+++ api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://admin:Admin1234Secure@localhost/hospital_db"
+DATABASE_URL = "mysql+pymysql://nsg_user:StrongPassword123@localhost/nsg_hospital"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
