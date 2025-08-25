41ffa6f51bf661c49dde77640326a8b369bf7688
whitedevil-141 <white.devil.dev.141@gmail.com>
2025-08-25 13:49:00 +0600

    updated endpoint and fixed db url

--- api/database.py
+++ api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://admin:Admin%401234@localhost/hospital_db"
+DATABASE_URL = "mysql+pymysql://admin:StrongPass123@localhost/hospital_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
