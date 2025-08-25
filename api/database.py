726de034568fa64c917db526e76df710145f1bed
whitedevil-141 <white.devil.dev.141@gmail.com>
2025-08-25 14:51:20 +0600

    Update the db again

--- api/database.py
+++ api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://nsg_user:StrongPassword123@localhost/nsg_hospital"
+DATABASE_URL = "mysql+pymysql://admin:U4p79y8Xq2%21@localhost/nsg_hospital"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
