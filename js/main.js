1cc89db76526f8d104cf9dccc0b3ac730e32c823
whitedevil-141 <white.devil.dev.141@gmail.com>
2025-09-23 11:55:29 +0600

    Update complain link text in mobile menu

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ if (desktopMenu && mobileMenu.innerHTML.trim() === "") {
 
   // add complain link as an <li> inside the cloned ul
   const complainLink = document.createElement("li");
-  complainLink.innerHTML = '<a href="index.html#contact">Give a Complain</a>';
+  complainLink.innerHTML = '<a href="index.html#contact">Complain/Suggestion</a>';
   clonedMenu.appendChild(complainLink);
 
   // insert the cloned ul into the mobile menu container
