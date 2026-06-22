dc1a78ec148be6c67ef4d4be120a0bd784f0df0a
WHITE DEVIL <white.devil.dev.141@gmail.com>
2026-06-22 12:13:07 +0600

    fixed the api endpoint

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Global API configuration - change the base URL here
-window.NSGH_API_BASE = window.NSGH_API_BASE || 'https://api.nsghbd.com/';
+window.NSGH_API_BASE = window.NSGH_API_BASE || 'https://api.nsghbd.com';
