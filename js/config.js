bc9a46692c2e36862bb849fd9bdd09786418596d
WHITE DEVIL <white.devil.dev.141@gmail.com>
2026-06-22 12:10:20 +0600

    Updated Api endpoint

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Global API configuration - change the base URL here
-window.NSGH_API_BASE = window.NSGH_API_BASE || 'http://127.0.0.1:8000';
+window.NSGH_API_BASE = window.NSGH_API_BASE || 'https://api.nsghbd.com/';
